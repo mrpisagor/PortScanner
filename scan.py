ff2cfc640df94381e8e1a45af70da816db3cc12c
mrpisagor <koc.ataberk02@gmail.com>
2024-06-19 20:38:49 +0300

    Some mistakes solved

--- scan.py
+++ scan.py
@@ -1,56 +1,121 @@
+#!/usr/bin/env python3
+
 import curses
 import argparse
-import time
+import socket
+import sys
 import pyfiglet
-from exceptions import MultiSelectedError, NullResultError 
+import re
 from scanner import Scanner
 
+
 def parse_arguments():
-    
     parse = argparse.ArgumentParser(
-    description="You can scan your given ports and you can see ports status and service names")
+        description="You can scan your given ports and you can see ports status and service names")
 
     src = parse.add_argument_group("Input")
-    src.add_argument("-t", "--target", metavar="Target", help="Target host",required=True)
+    src.add_argument("-t", "--target", metavar="Target", help="Target host", required=True)
     src.add_argument("-p", "--port", metavar="Port",
-                 help="You can type one port or more like p1,p2,p3... "
-                      "if you want to give range you have to type like MIN_PORT MAX_PORT ",
-                 type=str, nargs="+",required=True)
-    src.add_argument("--thread", help="Thread number you can select between 1 and 500 if you don't select default is 200",
-                 type=int)
-    src.add_argument("--limit",help="don't show ports more than limit",type=int)
+                     help="You can type one port or more like p1,p2,p3... "
+                          "if you want to give range you have to type like MIN_PORT MAX_PORT ",
+                     type=str, nargs="+")
+    src.add_argument("--thread",
+                     help="Thread number you can select between 1 and 500 if you don't select default is 200",
+                     default=200,
+                     type=int)
+    src.add_argument("--limit", help="don't show ports more than limit", type=int)
     src.add_argument("-g", "--get", metavar="Read", help="Gets ports from .txt file")
     output = parse.add_argument_group("Output")
     output.add_argument("-w", "--write", metavar="Write", help="Write result to .txt or .json")
 
     args = parse.parse_args()
-    
+
     return args
+
+
+def get_ports_from_file(file_name):
+    with open(file_name, "r") as f:
+        data = [*map(lambda x: x.replace("\n", ""), f.readlines())]
+        data = ",".join(data)
+        if data:
+            return data
+        else:
+            return None
+
+
+def check_ip_valid(host):
+    ip_format = re.compile(r"^[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$")
+    host = socket.gethostbyname(host)
+    return True if ip_format.match(host) else False
+
+
+def parse_ports(scr, ports: list):
+    if len(ports) == 2 and all(map(lambda x: x.isdecimal() and 0 <= int(x) <= 65535, ports)):
+        isrange = True
+    elif all(map(lambda x: x.isdecimal() and 0 <= int(x) <= 65535, "".join(ports).replace(" ", "").split(","))):
+        isrange = False
+    else:
+        scr.addstr(" Please enter valid arguments\n ")
+        scr.refresh()
+        scr.getch()
+        curses.endwin()
+        sys.exit(1)
+
+    port_list = [*map(lambda x: int(x), "".join(ports).split(","))] if not isrange else range(int(ports[0]),
+                                                                                              int(ports[1]) + 1)
+
+    return port_list
+
 
 def main():
     args = parse_arguments()
     scr = curses.initscr()
-    scr.addstr(pyfiglet.figlet_format("  PortScanner V 1.2",width=110)+"\n\n")
+    scr.addstr(pyfiglet.figlet_format("  PortScanner V 1.3", width=110) + "\n\n")
     scr.refresh()
+
+    if not check_ip_valid(args.target):
+        scr.addstr(" Please enter valid ipv4 address\n ")
+        scr.refresh()
+        scr.getch()
+        curses.endwin()
+        sys.exit(1)
+
+    ports = args.port
+
+    if ports is None and args.get is None:
+        scr.addstr(" Please add give ports!\n ")
+        scr.refresh()
+        scr.getch()
+        curses.endwin()
+        sys.exit(1)
     if args.get:
         if args.port:
-            raise MultiSelectedError("You cannot get ports from more then one options!")
-        try:
-            with open(args.get, "r") as file:
-                data = [*map(lambda x: x.replace("\n", ""), file.readlines())]
-                data = ",".join(data)
-                if data:
-                    args.port = [data]
-                else:
-                    raise NullResultError("File is empty!")
-        except OSError:
-            raise FileNotFoundError("File not found!")
+            scr.addstr(" You cannot get ports from more then one options!\n ")
+            scr.refresh()
+            scr.getch()
+            curses.endwin()
+            sys.exit(1)
+        else:
+            ports = get_ports_from_file(args.get)
+            if ports is None:
+                scr.addstr(" File is empty\n ")
+                scr.refresh()
+                scr.getch()
+                curses.endwin()
+                sys.exit(1)
 
-    if args.port and args.target:
-        scanner = Scanner(args.thread,args.limit)
-        scanner.put(args.target, args.port)
-        result = scanner.scan(scr)
-        result.parse_and_print(args)
+    scanner = Scanner(args.thread, args.limit)
+    ports = parse_ports(scr, ports)
+    scanner.put(args.target, ports)
+    result = scanner.scan(scr)
+
+    result.print_status()
+
+    if args.write:
+        result.write_to_file(args.write)
+
+    scr.getch()
+    curses.endwin()
 
 
 if __name__ == "__main__":
--- scanner.py
+++ scanner.py
@@ -1,39 +1,40 @@
+import queue
 from queue import Queue
 from threading import Thread
 import socket
 import re
-from exceptions import NotExistsIpError 
+from exceptions import InvalidThreadCountError
 from portstatus import PortStatus
 import time
 
 
 class Scanner:
 
-    def __init__(self,thread_count,limit):
+    def __init__(self, thread_count, limit):
 
-        self.queue = None
-        self.port_list = None
+        self.queue: queue.Queue = None
         self.HOST = None
         self.server = None
         self.limit = limit
         self.ip_format = re.compile(r"^[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$")
-        if thread_count or thread_count == 0:
-            if thread_count < 1 or thread_count > 500:
-                raise NotExistsValueError("You have to select thread count between 1 and 500")
-            self.THREADS = thread_Count
-        else:
-            self.THREADS = 200
+        if thread_count < 1 or thread_count > 500:
+            raise InvalidThreadCountError("You have to select thread count between 1 and 500")
+        self.THREADS = thread_count
         self.ports = []
+
     def start_threads(self):
-        self.thread_list = [Thread(target=self.scanner,daemon=True) for _ in range(self.THREADS)]
+        self.thread_list = [Thread(target=self.scanner, daemon=True) for _ in range(self.THREADS)]
         for thread in self.thread_list:
             thread.start()
+
     def wait_threads(self):
         for thread in self.thread_list:
             thread.join()
 
-    def scan(self,scr):
-        scr.addstr(f"  Scanning starting on host {self.HOST}...")
+    def scan(self, scr):
+        scr.addstr(f"  Scanning started on host {self.HOST}...\n")
+        scr.addstr(
+            "\n\n" + " " * 4 + "Port" + " " * 8 + "Status" + " " * 8 + "Service\n" + " " * 2 + "-" * 38 + "\n")
         scr.refresh()
         self.start_threads()
         begin = time.time()
@@ -46,32 +47,18 @@ class Scanner:
             port = self.queue.get()
             try:
                 service = socket.getservbyport(port)
-            except socket.error:
+            except Exception:
                 service = "unknown"
             try:
                 self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.server.settimeout(3)
                 self.server.connect((self.HOST, port))
-
                 self.ports.append((port, service))
                 self.server.close()
             except socket.error:
                 pass
 
-    def put(self, host, port):
-        host = socket.gethostbyname(host)
-        if self.ip_format.match(host):
-            self.HOST = host
-        else:
-            raise NotExistsIpError("Doesn't match the ip address form")
-
-        if (not "".join(port).replace(",", "").isdecimal()) or ("," in "".join(port) and len(port) != 1) or (
-                not (len(port) == 2 or len(port) == 1)):
-            raise TypeError("Please enter valid arguments")
-
-        condition = ("," in "".join(port) or  "".join(port).isdecimal()) and  len(port) == 1
-        self.port_list = "".join(port).split(",") if condition else range(int(port[0]), int(port[1]) + 1)
+    def put(self, host, ports):
+        self.HOST = host
         self.queue = Queue()
-        for port in self.port_list:
-            self.queue.put(int(port))
-
+        for port in ports:
+            self.queue.put(port)
